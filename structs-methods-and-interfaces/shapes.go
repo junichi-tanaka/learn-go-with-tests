2db79a25b902fc87718b342c00a3f59a2bb628aa
Junichi TANAKA <junsan45@gmail.com>
2020-01-16 02:34:51 +0900

    [Structs, methods & interfaces] Write the minimal amount of code for the test to run and check the failing test output

--- structs-methods-and-interfaces/shapes.go
+++ structs-methods-and-interfaces/shapes.go
@@ -5,9 +5,15 @@ type Rectangle struct {
 	Height float64
 }
 
+type Circle struct {
+    Radius float64
+}
+
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
+
+func Area(circle Circle) float64 { return 0.0 }
 
 func Area(rectangle Rectangle) float64 {
 	return rectangle.Width * rectangle.Height
