56045cd7c6e89d6c41bbcd3ef845d2db761e6f6b
Junichi TANAKA <junsan45@gmail.com>
2020-01-20 01:07:39 +0900

    [Maps] Write enough code to make it pass

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -34,7 +34,9 @@ func (d Dictionary) Add(word, definition string) error {
 	return nil
 }
 
-func (d Dictionary) Update(word, definition string) {}
+func (d Dictionary) Update(word, definition string) {
+	d[word] = definition
+}
 
 func (e DictionaryErr) Error() string {
 	return string(e)
