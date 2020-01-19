991b72200192c884cfe02cba61505bcec9d886c6
Junichi TANAKA <junsan45@gmail.com>
2020-01-20 01:06:53 +0900

    [Maps] Write minimal amount of code for the test to run and check the failing test output

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -34,6 +34,8 @@ func (d Dictionary) Add(word, definition string) error {
 	return nil
 }
 
+func (d Dictionary) Update(word, definition string) {}
+
 func (e DictionaryErr) Error() string {
 	return string(e)
 }
