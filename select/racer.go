4b89c0287417b8d956d236d5b1f0f271aab8c50e
Junichi TANAKA <junsan45@gmail.com>
2020-01-24 02:51:55 +0900

    [Select] Write the minimal amount of code for the test to run and check the failing test output

--- select/racer.go
+++ select/racer.go
@@ -5,19 +5,26 @@ import (
 	"time"
 )
 
-func Racer(a, b string) (winner string) {
-	aDuration := measureResponseTime(a)
-	bDuration := measureResponseTime(b)
-
-	if aDuration < bDuration {
-		return a
+func Racer(a, b string) (winner string, error error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
 	}
-
-	return b
 }
 
 func measureResponseTime(url string) time.Duration {
 	start := time.Now()
 	http.Get(url)
 	return time.Since(start)
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
